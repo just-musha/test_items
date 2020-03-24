4b72f65d943b3a2d76ad6606495fa2db4672b487
Maria Bulatova <maria.r.bulatova@yandex.ru>
2020-03-24 16:07:00 +0300

    Fix test to use is_displayed() func

--- test_items.py
+++ test_items.py
@@ -1,10 +1,8 @@
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
+
 def test_busket_present_on_page(browser):
     browser.get(link)
-    buttonFound = True
-    try:
-        button = browser.find_element_by_css_selector("button.btn-add-to-basket44")
-    except:
-        buttonFound = False
-    assert buttonFound == True
+    assert browser.find_element_by_css_selector(
+        "button.btn-add-to-basket").is_displayed(), \
+        "Кнопка 'Добавить в корзину' отсутствует"
